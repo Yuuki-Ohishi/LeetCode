adf7667b93f97ece896ddef7a59ab1bf66376d76
Yuuki-Ohishi <yuuki0301pokemon@gmail.com>
2026-08-14 11:29:09 +0900

    Time: 63 ms (11.08%), Space: 24.4 MB (85.65%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -3,11 +3,10 @@ class Solution:
         right = 0
         left = 0
         zero_count = 0
-        element_count = 0
         max_length = 0
 
         #要素全てを確認するまでループ
-        while element_count < len(nums) :
+        while right < len(nums) :
             #要素が0の場合、カウント
             if nums[right] == 0:
                 zero_count += 1
@@ -19,16 +18,11 @@ class Solution:
                 left += 1
 
             #0を含まない要素数を計算
-            length = right - left + 1 - zero_count
+            length = right - left
             #0が1個以下で0を除いた最大の長さを保存
             max_length = max(max_length, length)
 
             right += 1
-            element_count += 1
-
-        #nums内に0が無かった時、1個削除
-        if zero_count == 0:
-            max_length -= 1
 
         return max_length
             
