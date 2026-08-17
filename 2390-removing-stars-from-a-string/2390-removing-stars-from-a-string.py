5dfb128a0c373a0b67906ec5fbce4a016800b05f
Yuuki-Ohishi <yuuki0301pokemon@gmail.com>
2026-08-17 11:20:34 +0900

    Time: 91 ms (60.41%), Space: 20.6 MB (19.17%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
@@ -1,13 +1,13 @@
 class Solution:
     def removeStars(self, s: str) -> str:
-        str_list = []
+        stack_list = []
 
         #*の時はリスト後ろから取り出し、それ以外はlist格納
         for char in s:
             if char == "*":
-                str_list.pop()
+                stack_list.pop()
             
             else:
-                str_list.append(char)
+                stack_list.append(char)
         
-        return "".join(str_list)
+        return "".join(stack_list)
